30adcf4f05cbd2a865928573bc2e41dc2b58fb65
snbrn <4136612@gmail.com>
2016-04-23 18:00:15 +0300

    Added files for SuperCool

--- TEST2/ViewController.swift
+++ TEST2/ViewController.swift
@@ -1,14 +1,22 @@
 //
 //  ViewController.swift
-//  Super Cool
+//  TEST2
 //
-//  Created by V on 13/04/16.
+//  Created by V on 18/04/16.
 //  Copyright © 2016 alanver. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var logo: UIImageView!
+    
+    @IBOutlet weak var bg: UIImageView!
+    @IBOutlet weak var push: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +28,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func button(sender: AnyObject) {
+        
+        logo.hidden = false
+        bg.hidden = false
+        push.hidden = true
+    }
 
 }
 
